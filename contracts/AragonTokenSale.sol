3146c931874441a03011489eeb8365e5ec7f02aa
Jorge Izquierdo <jorge@izqui.me>
2017-04-03 13:12:03 +0200

    Safe math and document code

--- contracts/AragonTokenSale.sol
+++ contracts/AragonTokenSale.sol
@@ -1,13 +1,14 @@
 pragma solidity ^0.4.6;
 
+import "zeppelin/SafeMath.sol";
 import "./interface/Controller.sol";
 import "./MiniMeToken.sol";
 
-contract AragonTokenSale is TokenController {
-    uint public initialTime;       // In UNIX Time Format
-    uint public finalTime;         // In UNIX Time Format
-    uint public totalCollected;         // In wei
-    bool public saleStopped;            // Safe stop
+contract AragonTokenSale is TokenController, SafeMath {
+    uint public initialBlock;             // Block number in which the sale starts
+    uint public finalBlock;               // Block number in which the sale end
+    uint public totalCollected;           // In wei
+    bool public saleStopped;              // Safe stop
     uint public initialPrice;
     uint public finalPrice;
     uint8 public priceStages;
@@ -22,8 +23,8 @@ contract AragonTokenSale is TokenController {
     uint public dust = 1 finney;        // Minimum investment
 
 /// @dev There are several checks to make sure the parameters are acceptable
-/// @param _initialTime The UNIX time that the sale will start
-/// @param _finalTime The UNIX time that the sale will end
+/// @param _initialBlock The Block number in which the sale starts
+/// @param _finalBlock The Block number in which the sale ends
 /// @param _aragonDevMultisig The address that will store the donated funds and manager
 /// for the sale
 /// @param _initialPrice The price for the first stage of the sale. Price in wei.
@@ -48,86 +49,127 @@ contract AragonTokenSale is TokenController {
             |      |    for priceStages = 4    |
             +------+---------------------------+-------->
                  Initial                     Final   time (s)
-                 time                        time
+                 block                       block
 Every stage is the same time length.
 Price increases by the same delta in every stage change
 
 */
 
     function AragonTokenSale (
-        uint _initialTime,
-        uint _finalTime,
+        uint _initialBlock,
+        uint _finalBlock,
         address _aragonDevMultisig,
         address _communityMultisig,
         uint256 _initialPrice,
         uint256 _finalPrice,
         uint8 _priceStages
     ) {
-        if ((_finalTime < now) ||
-            (_finalTime <= _initialTime) ||
+        if ((_finalBlock < block.number) ||
+            (_finalBlock <= _initialBlock) ||
             (_aragonDevMultisig == 0x0 || communityMultisig == 0x0) ||
             (_initialPrice > _finalPrice) ||
             (_priceStages < 1))
         {
           throw;
         }
-        initialTime = _initialTime;
-        finalTime = _finalTime;
+
+        // Save constructor arguments as global variables
+        initialBlock = _initialBlock;
+        finalBlock = _finalBlock;
         aragonDevMultisig = _aragonDevMultisig;
         communityMultisig = _communityMultisig;
         initialPrice = _initialPrice;
         finalPrice = _finalPrice;
         priceStages = _priceStages;
 
+        // Deploy contracts
         deployANT();
     }
 
-    function deployANT() {
+    // Deploy ANT is called by the contract only once to setup all the needed contracts.
+    function deployANT() private {
+      // Assert that the function hasn't been called before, as activate will happen at the end
+      if (activated[this]) throw;
+      // Deploy token factory that allows the token to clone itself
       MiniMeTokenFactory factory = new MiniMeTokenFactory();
+      // Assert that we knew where this first contract was going to be deployed.
       if (address(factory) != addressForContract(1)) throw;
 
-      // for not deploying the string to the blockchain before announcement
-      token = new MiniMeToken(address(factory), 0x0, 0, "ANT", 18, "ANT", true);
-      if (address(token) != addressForContract(2)) throw;
+      // TODO: Token name = 'Aragon Network Token'
+      // Kept as placeholder prior to announcement of the sale
+      token = new MiniMeToken(address(factory), 0x0, 0, "Token name", 18, "ANT", true);
+      if (address(token) != addressForContract(2)) throw; // Assert 2
 
       aragonNetwork = addressForContract(3); // network will eventually be deployed here
+
+      // Contract activates sale as all requirements are ready
+      // Important in case this function stops being called in the constructor for gas limits.
+      doActivateSale(this);
     }
 
+    // @notice Certain addresses need to call the activate function prior to the sale opening block.
+    // This proves that they have checked the sale contract is legit, as well as proving
+    // the capability for those addresses to interact with the contract.
     function activateSale() {
-      if (now >= _initialTime) throw;
-      activated[msg.sender] = true;
+      doActivateSale(msg.sender);
     }
 
+    function doActivateSale(address _entity) only_before_sale private {
+      activated[_entity] = true;
+    }
+
+    // @notice Whether the needed accounts have activated the sale.
+    // @return Is sale activated
     function isActivated() constant returns (bool) {
-      return activated[aragonDevMultisig] && activated[communityMultisig];
+      return activated[this] && activated[aragonDevMultisig] && activated[communityMultisig];
     }
 
-    function getPrice(uint date) constant returns (uint256) {
-      if (date < initialTime || date > finalTime) return 2**250;
+    // @notice Get the price for a ANT token at any given block number
+    // @param _blockNumber the block for which the price is requested
+    // @return Price in wei for 1 ANT token.
+    // If sale isn't ongoing for that block, returns a very high price 2^250 wei.
+    function getPrice(uint _blockNumber) constant returns (uint256) {
+      if (_blockNumber < initialBlock || _blockNumber > finalBlock) return 2**250;
 
-      return priceForStage(stageForDate(date));
+      return priceForStage(stageForDate(_blockNumber));
     }
 
-    function stageForDate(uint date) constant returns (uint8) {
-      return uint8(uint256(priceStages) * (date - initialTime) / (finalTime - initialTime));
+    // @notice Get what the stage is for a given blockNumber
+    // @param _blockNumber: Block number
+    // @return The sale stage for that block. Number is between 0 and (priceStages - 1)
+    function stageForDate(uint _blockNumber) constant returns (uint8) {
+      uint blockN = safeSub(_blockNumber, initialBlock);
+      uint totalBlocks = safeSub(finalBlock, initialBlock);
+
+      return uint8(safeDiv(safeMul(priceStages, blockN), totalBlocks));
     }
 
-    function priceForStage(uint8 stage) constant returns (uint256) {
-      uint256 stageDelta = (finalPrice - initialPrice) / uint256(priceStages - 1);
-      return initialPrice + uint256(stage) * stageDelta;
+    // @notice Get what the price is for a given stage
+    // @param _stage: Stage number
+    // @return Price in wei for that stage.
+    // If sale stage doesn't exist, returns a very high price 2^250 wei.
+    function priceForStage(uint8 _stage) constant returns (uint256) {
+      if (_stage >= priceStages) return 2**250;
+      uint priceDifference = safeSub(finalPrice, initialPrice);
+      uint stageDelta = safeDiv(priceDifference, uint(priceStages - 1));
+      return safeAdd(initialPrice, safeMul(uint256(_stage), stageDelta));
     }
 
-    function allocatePresaleTokens(address receiver, uint amount) only(aragonDevMultisig) {
-      if (now >= initialTime) throw;
-      if (!token.generateTokens(receiver, amount)) throw;
+    // @notice Aragon Dev needs to make initial token allocations for presale partners
+    // This allocation has to be made before the sale is activated. Activating the sale means no more
+    // arbitrary allocations are possible and expresses conformity.
+    // @param _receiver: The receiver of the tokens
+    // @param _amount: Amount of tokens allocated for receiver.
+    function allocatePresaleTokens(address _receiver, uint _amount) only_before_sale_activation only_before_sale only(aragonDevMultisig) {
+      if (!token.generateTokens(_receiver, _amount)) throw;
     }
 
-    function deployNetwork(bytes networkCode) only(communityMultisig) {
-      if (now <= finalTime || !saleStopped) throw;
-
+    // @notice Deploy Aragon Network contract.
+    // @param _networkCode: The network contract bytecode followed by its constructor args.
+    function deployNetwork(bytes _networkCode) only_after_sale only(communityMultisig) {
       address deployedAddress;
       assembly {
-        deployedAddress := create(0,add(networkCode,0x20), mload(networkCode))
+        deployedAddress := create(0,add(_networkCode,0x20), mload(_networkCode))
         jumpi(invalidJumpLabel,iszero(extcodesize(deployedAddress)))
       }
 
@@ -155,7 +197,7 @@ Price increases by the same delta in every stage change
 /// have the tokens created in an address of their choosing
 /// @param _owner The address that will hold the newly created tokens
 
-    function proxyPayment(address _owner) payable returns(bool) {
+    function proxyPayment(address _owner) payable returns (bool) {
       doPayment(_owner);
       return true;
     }
@@ -166,7 +208,7 @@ Price increases by the same delta in every stage change
 /// @param _to The destination of the transfer
 /// @param _amount The amount of the transfer
 /// @return False if the controller does not authorize the transfer
-    function onTransfer(address _from, address _to, uint _amount) returns(bool) {
+    function onTransfer(address _from, address _to, uint _amount) returns (bool) {
       return true;
     }
 
@@ -176,53 +218,51 @@ Price increases by the same delta in every stage change
 /// @param _spender The spender in the `approve()` call
 /// @param _amount The amount in the `approve()` call
 /// @return False if the controller does not authorize the approval
-    function onApprove(address _owner, address _spender, uint _amount) returns(bool) {
+    function onApprove(address _owner, address _spender, uint _amount) returns (bool) {
       return true;
     }
 
 /// @dev `doPayment()` is an internal function that sends the ether that this
-///  contract receives to the `vault` and creates tokens in the address of the
-///  `_owner` assuming the Campaign is still accepting funds
+///  contract receives to the aragonDevMultisig and creates tokens in the address of the
 /// @param _owner The address that will hold the newly created tokens
 
-    function doPayment(address _owner) internal {
-      if ((now < initialTime) || (now > finalTime)) throw;
-      if (saleStopped) throw;
-      if (!isActivated()) throw;
-      if (token.controller() != address(this)) throw;
-      if (msg.value < dust) throw;
+    function doPayment(address _owner) only_during_sale_period only_sale_not_stopped only_sale_activated internal {
+      if (token.controller() != address(this)) throw; // Check is token controller and able to allocate tokens
+      if (msg.value < dust) throw; // Check it is at least minimum investment
 
-      totalCollected += msg.value;
-      uint256 boughtTokens = msg.value / getPrice(now);
+      totalCollected = safeAdd(totalCollected, msg.value); // Save total collected amount
+      uint256 boughtTokens = safeDiv(msg.value, getPrice(block.number)); // Calculate how many tokens bought
 
-      if (!aragonDevMultisig.send(msg.value)) throw;
-      if (!token.generateTokens(_owner, boughtTokens)) throw;
-
-      return;
+      if (!aragonDevMultisig.send(msg.value)) throw; // Send funds to multisig
+      if (!token.generateTokens(_owner, boughtTokens)) throw; // Allocate tokens
     }
 
-/// @notice `finalizeFunding()` ends the Campaign by calling setting the
-///  controller to 0, thereby ending the issuance of new tokens and stopping the
-///  Campaign from receiving more ether
-/// @dev `finalizeFunding()` can only be called after the end of the funding period.
-
-    function emergencyStopSale() only(aragonDevMultisig) {
-      if (saleStopped) throw;
+    // @notice Function to stop sale for an emergency.
+    // @dev Only Aragon Dev can do it after it has been activated.
+    function emergencyStopSale() only_sale_activated only_sale_not_stopped only(aragonDevMultisig) {
       saleStopped = true;
     }
 
-    function restartSale() only(aragonDevMultisig) {
-      if (now > finalTime) throw;
+    // @notice Function to restart stopped sale.
+    // @dev Only Aragon Dev can do it after it has been disabled and during the sale period.
+    function restartSale() only_during_sale_period only_sale_stopped only(aragonDevMultisig) {
       saleStopped = false;
     }
 
-    function finalizeSale() only(aragonDevMultisig) {
-      if (now < finalTime) throw;
+    // @notice Finalizes sale generating the tokens for Aragon Dev.
+    // @dev Transfer the token controller power to the future Aragon Network,
+    // as until it is deployed it will be a non contract, no more modifications
+    // on the token can be made until deploying the network.
 
-      uint256 aragonTokens = token.totalSupply() / 4; // So it is 20% of the total number of tokens
+    function finalizeSale() only(aragonDevMultisig) {
+      if (block.number < finalBlock) throw;
+
+      // Aragon Dev owns 25% of the total number of emitted tokens.
+      uint256 aragonTokens = token.totalSupply() / 3;
       if (!token.generateTokens(aragonDevMultisig, aragonTokens)) throw;
-      saleStopped = true;
       token.changeController(aragonNetwork);
+
+      saleStopped = true;
     }
 
     function setAragonDevMultisig(address _newMultisig) only(aragonDevMultisig) {
@@ -237,4 +277,42 @@ Price increases by the same delta in every stage change
       if (msg.sender != x) throw;
       _;
     }
+
+    modifier only_before_sale {
+      if (block.number >= initialBlock) throw;
+      _;
+    }
+
+    modifier only_during_sale_period {
+      if (block.number < initialBlock) throw;
+      if (block.number > finalBlock) throw;
+      _;
+    }
+
+    modifier only_sale_stopped {
+      if (!saleStopped) throw;
+      _;
+    }
+
+    modifier only_sale_not_stopped {
+      if (saleStopped) throw;
+      _;
+    }
+
+    modifier only_before_sale_activation {
+      if (isActivated()) throw;
+      _;
+    }
+
+    modifier only_sale_activated {
+      if (!isActivated()) throw;
+      _;
+    }
+
+    modifier only_after_sale {
+      if (block.number <= finalBlock) throw;
+      if (!saleStopped) throw;
+      _;
+    }
+
 }
