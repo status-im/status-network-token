9ba9ae29783bbd595e1f70cfa8bb47b305bc5c81
Jordi Baylina <jordi@baylina.cat>
2017-05-21 15:58:39 +0200

    SafeMath in dynamicCeiling

--- contracts/DynamicCeiling.sol
+++ contracts/DynamicCeiling.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.11;
 
-contract DynamicCeiling {
+import "./SafeMath.sol";
+
+contract DynamicCeiling is SafeMath {
 
     struct CurvePoint {
         bytes32 hash;
@@ -23,7 +25,7 @@ contract DynamicCeiling {
         if (points.length > 0) throw;
         uint i;
         points.length = _pointHashes.length;
-        for (i=0; i< _pointHashes.length; i++) {
+        for (i=0; i< _pointHashes.length; i = safeAdd(i,1)) {
             points[i].hash = _pointHashes[i];
         }
     }
@@ -33,13 +35,13 @@ contract DynamicCeiling {
         if (allRevealed) throw;
         if (points[revealedPoints].hash != sha3(_block, _limit, _last, _salt)) throw;
         if (revealedPoints > 0) {
-            if (_block <= points[revealedPoints - 1].block) throw;
-            if (_limit < points[revealedPoints - 1].limit) throw;
+            if (_block <= points[safeSub(revealedPoints, 1)].block) throw;
+            if (_limit < points[safeSub(revealedPoints, 1)].limit) throw;
         }
         points[revealedPoints].block = _block;
         points[revealedPoints].limit = _limit;
         points[revealedPoints].revealed = true;
-        revealedPoints ++;
+        revealedPoints = safeAdd(revealedPoints, 1);
         if (_last) allRevealed = true;
     }
 
@@ -47,15 +49,15 @@ contract DynamicCeiling {
         if (revealedPoints == 0) return 0;
 
         // Shortcut for the actual value
-        if (_block >= points[revealedPoints-1].block)
-            return points[revealedPoints-1].limit;
+        if (_block >= points[safeSub(revealedPoints,1)].block)
+            return points[safeSub(revealedPoints,1)].limit;
         if (_block < points[0].block) return 0;
 
         // Binary search of the value in the array
         uint min = 0;
-        uint max = revealedPoints-1;
-        while (max != min + 1) {
-            uint mid = (max + min)/ 2;
+        uint max = safeSub(revealedPoints,1);
+        while (max != safeAdd(min, 1)) {
+            uint mid = safeDiv(safeAdd(max, min), 2);
             if (points[mid].block<=_block) {
                 min = mid;
             } else {
@@ -63,10 +65,14 @@ contract DynamicCeiling {
             }
         }
 
-        return points[min].limit  +
-            (_block - points[min].block) *
-                ( points[max].limit - points[min].limit ) /
-                ( points[max].block - points[min].block );
+        return safeAdd(
+                    points[min].limit,
+                    safeDiv(
+                        safeMul(
+                            safeSub(_block, points[min].block),
+                            safeSub(points[max].limit, points[min].limit)),
+                        safeSub(points[max].block, points[min].block)));
+
     }
 
     function calculateHash(uint _block, uint _limit, bool _last, uint _salt) constant returns (bytes32) {
