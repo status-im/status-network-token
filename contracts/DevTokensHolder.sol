6a7ac47894fc5c882b989a00f9b376a3689ec343
Jordi Baylina <jordi@baylina.cat>
2017-05-21 22:43:38 +0200

    Dont allow devs manipulate their tokens

--- contracts/DevTokensHolder.sol
+++ contracts/DevTokensHolder.sol
@@ -61,6 +61,7 @@ contract DevTokensHolder is Owned, SafeMath {
     /// @param _token The address of the token contract that you want to recover
     ///  set to 0 in case you want to extract ether.
     function claimTokens(address _token) onlyOwner {
+      if (_token == address(snt)) throw;
       if (_token == 0x0) {
           owner.transfer(this.balance);
           return;
