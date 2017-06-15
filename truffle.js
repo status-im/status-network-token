8cdd01ff55eecd9a1671546c0fc413587173ccb9
nfnty <git@nfnty.se>
2017-06-15 22:22:49 +0200

    Add migration scripts

--- truffle.js
+++ truffle.js
@@ -1,9 +1,8 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 
 const mnemonic = process.env.TEST_MNEMONIC || 'status mnemonic status mnemonic status mnemonic status mnemonic status mnemonic status mnemonic';
-const providerDevelopment = new HDWalletProvider(mnemonic, 'http://localhost:8545', 0);
-const providerRopsten = new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/', 1);
-const providerKovan = new HDWalletProvider(mnemonic, 'https://kovan.infura.io', 2);
+const providerRopsten = new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/', 0);
+const providerKovan = new HDWalletProvider(mnemonic, 'https://kovan.infura.io', 0);
 
 module.exports = {
     networks: {
@@ -16,7 +15,8 @@ module.exports = {
         },
         development_migrate: {
             network_id: 15,
-            provider: providerDevelopment,
+            host: "localhost",
+            port: 8545,
             gas: 4000000,
             gasPrice: 4e9,
             from: "0xf93df8c288b9020e76583a6997362e89e0599e99",
