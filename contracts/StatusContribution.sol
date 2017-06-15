a6a20129994b016958042256141baa7fb08ad298
nfnty <git@nfnty.se>
2017-06-15 08:22:39 +0200

    contracts: StatusContribution: Rename `cap` to `toCollect`

--- contracts/StatusContribution.sol
+++ contracts/StatusContribution.sol
@@ -219,11 +219,11 @@ contract StatusContribution is Owned, TokenController {
     }
 
     function buyGuaranteed(address _th) internal {
-        uint256 toFund;
-        uint256 cap = guaranteedBuyersLimit[_th];
+        uint256 toCollect = guaranteedBuyersLimit[_th];
 
-        if (guaranteedBuyersBought[_th].add(msg.value) > cap) {
-            toFund = cap.sub(guaranteedBuyersBought[_th]);
+        uint256 toFund;
+        if (guaranteedBuyersBought[_th].add(msg.value) > toCollect) {
+            toFund = toCollect.sub(guaranteedBuyersBought[_th]);
         } else {
             toFund = msg.value;
         }
