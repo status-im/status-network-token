afe91dd449f2f8f30af2557f4f4bfbac1ad56e9c
Marc <marc.d.griffiths@gmail.com>
2017-06-18 14:12:15 +0800

    tests: Adding new tests to claim external

--- test/claimExternal.js
+++ test/claimExternal.js
@@ -1,11 +1,11 @@
-// Simulate a an external claim
+// Simulate a full contribution
 
-const MultiSigWallet = artifacts.require("MultiSigWallet");
 const MiniMeTokenFactory = artifacts.require("MiniMeTokenFactory");
 const SGT = artifacts.require("SGT");
 const SNT = artifacts.require("SNT");
-const StatusContributionMock = artifacts.require("StatusContributionMock");
+const MultiSigWallet = artifacts.require("MultiSigWallet");
 const ContributionWallet = artifacts.require("ContributionWallet");
+const StatusContributionMock = artifacts.require("StatusContributionMock");
 const DevTokensHolder = artifacts.require("DevTokensHolderMock");
 const SGTExchanger = artifacts.require("SGTExchanger");
 const DynamicCeiling = artifacts.require("DynamicCeiling");
@@ -15,18 +15,12 @@ const ExternalToken = artifacts.require("ExternalToken");
 const setHiddenCurves = require("./helpers/hiddenCurves.js").setHiddenCurves;
 const assertFail = require("./helpers/assertFail");
 
-contract("StatusContribution", function(accounts) {
-    const addressStatus = accounts[0];
-    const addressCommunity = accounts[1];
-    const addressReserve = accounts[2];
-    const addressDevs = accounts[3];
-    const addressSGTHolder = accounts[4];
-
+contract("StatusContribution", (accounts) => {
     let multisigStatus;
-    let multisigCommunity;
-    let multisigReserve;
+    let multisigComunity;
+    let multisigSecondarySell;
     let multisigDevs;
-    let miniMeTokenFactory;
+    let miniMeFactory;
     let sgt;
     let snt;
     let statusContribution;
@@ -38,27 +32,23 @@ contract("StatusContribution", function(accounts) {
     let externalToken;
 
     const curves = [
-        [web3.toWei(3), 30, 10**12],
-        [web3.toWei(13), 30, 10**12],
-        [web3.toWei(15), 30, 10**12],
+        [web3.toWei(3)],
+        [web3.toWei(13)],
+        [web3.toWei(15)],
     ];
     const startBlock = 1000000;
     const endBlock = 1003000;
 
-    const maxSGTSupply = 5000 * 2;
+    it("Should deploy Contribution contracts", async () => {
+        multisigStatus = await MultiSigWallet.new([accounts[0]], 1);
+        multisigComunity = await MultiSigWallet.new([accounts[1]], 1);
+        multisigSecondarySell = await MultiSigWallet.new([accounts[2]], 1);
+        multisigDevs = await MultiSigWallet.new([accounts[3]], 1);
+        miniMeFactory = await MiniMeTokenFactory.new();
+        sgt = await SGT.new(miniMeFactory.address);
+        await sgt.generateTokens(accounts[4], 5000);
 
-    it("Deploys all contracts", async function() {
-        multisigStatus = await MultiSigWallet.new([addressStatus], 1);
-        multisigCommunity = await MultiSigWallet.new([addressCommunity], 1);
-        multisigReserve = await MultiSigWallet.new([addressReserve], 1);
-        multisigDevs = await MultiSigWallet.new([addressDevs], 1);
-
-        miniMeTokenFactory = await MiniMeTokenFactory.new();
-
-        sgt = await SGT.new(miniMeTokenFactory.address);
-        await sgt.generateTokens(addressSGTHolder, 5000);
-
-        snt = await SNT.new(miniMeTokenFactory.address);
+        snt = await SNT.new(miniMeFactory.address);
         statusContribution = await StatusContributionMock.new();
         contributionWallet = await ContributionWallet.new(
             multisigStatus.address,
@@ -69,57 +59,69 @@ contract("StatusContribution", function(accounts) {
             statusContribution.address,
             snt.address);
         sgtExchanger = await SGTExchanger.new(sgt.address, snt.address, statusContribution.address);
-        dynamicCeiling = await DynamicCeiling.new(addressStatus, statusContribution.address);
+        dynamicCeiling = await DynamicCeiling.new(accounts[0], statusContribution.address);
 
         await setHiddenCurves(dynamicCeiling, curves);
 
         sntPlaceHolder = await SNTPlaceHolderMock.new(
-            multisigCommunity.address,
+            multisigComunity.address,
             snt.address,
             statusContribution.address,
             sgtExchanger.address);
 
         await snt.changeController(statusContribution.address);
+        await sgt.changeController(sgtExchanger.address);
 
         await statusContribution.initialize(
             snt.address,
-            sntPlaceHolder.address,
-
             startBlock,
             endBlock,
-
             dynamicCeiling.address,
 
             contributionWallet.address,
 
-            multisigReserve.address,
-            sgtExchanger.address,
             devTokensHolder.address,
 
+            multisigSecondarySell.address,
             sgt.address,
-            maxSGTSupply);
+
+            sgtExchanger.address,
+            5000 * 2,
+
+            sntPlaceHolder.address);
 
         externalToken = await ExternalToken.new();
-        await externalToken.generateTokens(addressStatus, 1000);
+        await externalToken.generateTokens(accounts[0], 1000);
     });
 
-    it("Sends to and recover tokens from the StatusContribution", async function() {
+    it("Should send and recover tokens to the StatusContribution", async () => {
         await externalToken.transfer(statusContribution.address, 100);
-        const balanceBefore = await externalToken.balanceOf(addressStatus);
+        const balanceBefore = await externalToken.balanceOf(accounts[0]);
         assert.equal(balanceBefore.toNumber(), 900);
 
         await statusContribution.claimTokens(externalToken.address);
-        const afterBefore = await externalToken.balanceOf(addressStatus);
+        const afterBefore = await externalToken.balanceOf(accounts[0]);
         assert.equal(afterBefore.toNumber(), 1000);
     });
 
-    it("Recovers tokens sent to SNT", async function() {
+    it("Should recover tokens sent to SNT", async () => {
         await externalToken.transfer(snt.address, 100);
-        const balanceBefore = await externalToken.balanceOf(addressStatus);
+        const balanceBefore = await externalToken.balanceOf(accounts[0]);
         assert.equal(balanceBefore.toNumber(), 900);
 
         await statusContribution.claimTokens(externalToken.address);
-        const afterBefore = await externalToken.balanceOf(addressStatus);
+        const afterBefore = await externalToken.balanceOf(accounts[0]);
         assert.equal(afterBefore.toNumber(), 1000);
     });
+
+    it("Should send and recover tokens to the SGTExchanger", async () => {
+        await externalToken.transfer(sgtExchanger.address, 100);
+        const balanceBefore = await externalToken.balanceOf(accounts[0]);
+        assert.equal(balanceBefore.toNumber(), 900);
+
+        await sgtExchanger.claimTokens(externalToken.address);
+        const afterBefore = await externalToken.balanceOf(accounts[0]);
+        assert.equal(afterBefore.toNumber(), 1000);
+    });
+
 });
